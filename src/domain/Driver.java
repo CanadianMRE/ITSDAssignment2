c286aceb40690f81f4e25ec4ef5507bdffeb8c7b
gursh <gursh@GG-PC>
2023-07-19 16:52:44 -0600

    sample1.xml works but not sample2.xml

--- src/domain/Driver.java
+++ src/domain/Driver.java
@@ -1,14 +1,13 @@
 package domain;
 
 public class Driver {
-    public static void main(String[] args) {
-        if (args.length < 1) {
-            System.out.println("Please provide the name of the XML file.");
+	public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Please provide the XML file name as a command line argument.");
             return;
         }
 
         String fileName = args[0];
-
         XMLParser parser = new XMLParser();
         parser.parseXML(fileName);
     }
