ce9e97240ee918a1610e79e913f11672c6c20958
gursh <gursh@GG-PC>
2023-07-20 09:34:04 -0600

    need the paths to work with spaces

--- src/domain/Driver.java
+++ src/domain/Driver.java
@@ -2,11 +2,17 @@ package domain;
 
 public class Driver {
 	public static void main(String[] args) {
+		System.out.println("This program is an XML parser. It reads an XML file, processes its contents, and performs the following tasks:\n" +
+		        "- Checks the validity of XML tags by verifying opening and closing tags.\n" +
+		        "- Identifies any errors in the XML structure, such as mismatched tags or extra/missing closing tags.\n" +
+		        "- Prints the parsed content of the XML file.\n" +
+		        "- Provides a summary of any errors found during the parsing process.");
+		
         if (args.length == 0) {
             System.out.println("Please provide the XML file name as a command line argument."
-            		          + "If the file is in res folder just type in res/filename.xml"
-            		          + "If the file is in a file path of your own type in yourpath/filename.xml if your path has spaces please put quotes around the path"
-            		          + "your path means the path in your computer and filename.xml means the name of your filename");
+            		          + "\nIf the file is in res folder just type in res/filename.xml"
+            		          + "\nIf the file is in a file path of your own type in yourpath/filename.xml if your path has spaces please put quotes around the path"
+            		          + "\nyour path means the path in your computer and filename.xml means the name of your filename");
             return;
         }
 
