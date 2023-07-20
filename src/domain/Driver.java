7fe1abfed53eeb8ab0699ae898de9b8c6f86c866
gursh <gursh@GG-PC>
2023-07-20 09:29:49 -0600

    res/ path works and absolute path works aswell just need to fix the white space when there is space in path

--- src/domain/Driver.java
+++ src/domain/Driver.java
@@ -3,7 +3,10 @@ package domain;
 public class Driver {
 	public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("Please provide the XML file name as a command line argument.");
+            System.out.println("Please provide the XML file name as a command line argument."
+            		          + "If the file is in res folder just type in res/filename.xml"
+            		          + "If the file is in a file path of your own type in yourpath/filename.xml if your path has spaces please put quotes around the path"
+            		          + "your path means the path in your computer and filename.xml means the name of your filename");
             return;
         }
 
--- src/domain/XMLParser.java
+++ src/domain/XMLParser.java
@@ -6,147 +6,151 @@ import utilities.MyQueue;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Paths;
 
 public class XMLParser {
-    private MyStack<String> tagStack;
-    private MyQueue<String> errorQueue;
-    private MyQueue<String> parsedFileQueue;
+	private MyStack<String> tagStack;
+	private MyQueue<String> errorQueue;
+	private MyQueue<String> parsedFileQueue;
 
-    public XMLParser() {
-        tagStack = new MyStack<>();
-        errorQueue = new MyQueue<>();
-        parsedFileQueue = new MyQueue<>();
-    }
+	public XMLParser() {
+		tagStack = new MyStack<>();
+		errorQueue = new MyQueue<>();
+		parsedFileQueue = new MyQueue<>();
+	}
 
-    public void parseXML(String fileName) {
-        String filePath = "res/" + fileName;
+	public void parseXML(String fileName) {
+		String filePath;
+		if (fileName.startsWith("res/")) {
+			filePath = fileName;
+		} else {
+			filePath = Paths.get(fileName).toAbsolutePath().toString();
+		}
 
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            int lineNumber = 1; // Variable to track the line number
-            while ((line = br.readLine()) != null) {
-                processLine(line.trim(), lineNumber); // Pass the line number to the processLine method
-                lineNumber++; // Increment the line number
-            }
-        } catch (IOException e) {
-            System.out.println("Error reading the XML file: " + e.getMessage());
-        }
+		try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+			String line;
+			int lineNumber = 1; // Variable to track the line number
+			while ((line = br.readLine()) != null) {
+				processLine(line.trim(), lineNumber); // Pass the line number to the processLine method
+				lineNumber++; // Increment the line number
+			}
+		} catch (IOException e) {
+			System.out.println("Error reading the XML file: " + e.getMessage() +
+					" \nPlease follow the right instructions that are shown in the Driver class");
+		}
 
-        matchRemainingTags();
+		matchRemainingTags();
 
-        printErrors();
-    }
+		printErrors();
+	}
 
-    private void processLine(String line, int lineNumber) {
-    	
-    	 if (line.startsWith("<?") && line.endsWith("?>")) {
-            parsedFileQueue.enqueue(line);
-        } else if (line.startsWith("<")) {
-            int startIndex = 0;
-            int openingBracketIndex = line.indexOf('<', startIndex);
-            int closingBracketIndex = line.indexOf('>', openingBracketIndex);
+	private void processLine(String line, int lineNumber) {
 
-            while (openingBracketIndex >= 0 && closingBracketIndex >= 0) {
-                String tag = line.substring(openingBracketIndex + 1, closingBracketIndex).trim();
-                if (tag.startsWith("/")) {
-                    processEndTag(tag.substring(1).trim(), lineNumber);
-                } else if (tag.endsWith("/")) {
-                    processSelfClosingTag(tag.substring(0, tag.length() - 1).trim(), lineNumber);
-                } else if (line.endsWith(">>")) {
-            		 tag = line.substring(line.indexOf("<"), line.lastIndexOf(">")+1);
-            		System.out.println("new tag: "+ tag);
-            		processStartTag(tag, lineNumber);
-            	}else{
-                    processStartTag(tag, lineNumber);
-                }
+		if (line.startsWith("<?") && line.endsWith("?>")) {
+			parsedFileQueue.enqueue(line);
+		} else if (line.startsWith("<")) {
+			int startIndex = 0;
+			int openingBracketIndex = line.indexOf('<', startIndex);
+			int closingBracketIndex = line.indexOf('>', openingBracketIndex);
 
-                startIndex = closingBracketIndex + 1;
-                openingBracketIndex = line.indexOf('<', startIndex);
-                closingBracketIndex = line.indexOf('>', openingBracketIndex);
-            }
+			while (openingBracketIndex >= 0 && closingBracketIndex >= 0) {
+				String tag = line.substring(openingBracketIndex + 1, closingBracketIndex).trim();
+				if (tag.startsWith("/")) {
+					processEndTag(tag.substring(1).trim(), lineNumber);
+				} else if (tag.endsWith("/")) {
+					processSelfClosingTag(tag.substring(0, tag.length() - 1).trim(), lineNumber);
+				} else if (line.endsWith(">>")) {
+					tag = line.substring(line.indexOf("<"), line.lastIndexOf(">") + 1);
+					System.out.println("new tag: " + tag);
+					processStartTag(tag, lineNumber);
+				} else {
+					processStartTag(tag, lineNumber);
+				}
 
-            parsedFileQueue.enqueue(line);
-        }
-    }
+				startIndex = closingBracketIndex + 1;
+				openingBracketIndex = line.indexOf('<', startIndex);
+				closingBracketIndex = line.indexOf('>', openingBracketIndex);
+			}
 
+			parsedFileQueue.enqueue(line);
+		}
+	}
 
-   
-    private void processSelfClosingTag(String tag, int lineNumber) {
-    	   if (tag.contains(" ")) {
-               // Start tag has attributes, extract the tag name without attributes
-               int spaceIndex = tag.indexOf(" ");
-               tag = tag.substring(0, spaceIndex);
-               
-           }
-    	    System.out.println("Self closing tag: <" + tag + "/>");
-    	}
+	private void processSelfClosingTag(String tag, int lineNumber) {
+		if (tag.contains(" ")) {
+			// Start tag has attributes, extract the tag name without attributes
+			int spaceIndex = tag.indexOf(" ");
+			tag = tag.substring(0, spaceIndex);
 
-    
+		}
+		System.out.println("Self closing tag: <" + tag + "/>");
+	}
 
-    private void processStartTag(String tag, int lineNumber) {
-    	 if (tag.contains(">")) {
-         	System.out.println("start tag on the process:" + tag);
-       	   errorQueue.enqueue("Line " + lineNumber + ": " + tag + " tag has an extra > symbol");
-       	   tag = tag.substring(tag.indexOf("<") + 1, tag.indexOf(" "));
-       	System.out.println("start tag after the process:" + tag);
-          }
-        // Check if the start tag has attributes by searching for a space
-        if (tag.contains(" ")) {
-            // Start tag has attributes, extract the tag name without attributes
-            int spaceIndex = tag.indexOf(" ");
-            tag = tag.substring(0, spaceIndex);
-           
-        }
-        
-        System.out.println("start tag: <" + tag + ">");
-        tagStack.push(tag);
-    }
+	private void processStartTag(String tag, int lineNumber) {
+		if (tag.contains(">")) {
+			System.out.println("start tag on the process:" + tag);
+			errorQueue.enqueue("Line " + lineNumber + ": " + tag + " tag has an extra > symbol");
+			tag = tag.substring(tag.indexOf("<") + 1, tag.indexOf(" "));
+			System.out.println("start tag after the process:" + tag);
+		}
+		// Check if the start tag has attributes by searching for a space
+		if (tag.contains(" ")) {
+			// Start tag has attributes, extract the tag name without attributes
+			int spaceIndex = tag.indexOf(" ");
+			tag = tag.substring(0, spaceIndex);
 
-    private void processEndTag(String tag, int lineNumber) {
-    	System.out.println("end tag: </" + tag + ">");
-        if (tagStack.isEmpty()) {
-            errorQueue.enqueue("Line " + lineNumber + ": </" + tag + "> tag has no matching opening tag.");
-        } else {
-            String topTag = tagStack.peek();
-            if (!topTag.equalsIgnoreCase(tag)) {
-                if (tagStack.contains(tag)) {
-                    while (!tagStack.isEmpty() && !tagStack.peek().equalsIgnoreCase(tag)) {
-                        String extraTag = tagStack.pop();
-                        errorQueue.enqueue("Line " + lineNumber + ": <" + extraTag + "> tag is extra and does not have a matching closing tag.");
-                    }
-                    if (!tagStack.isEmpty()) {
-                        tagStack.pop(); // Pop the matching opening tag
-                    }
-                } else {
-                    errorQueue.enqueue("Line " + lineNumber + ": </" + tag + "> tag does not have a matching opening tag.");
-                }
-            } else {
-                tagStack.pop(); // Pop the matching opening tag
-            }
-        }
-    }
+		}
 
+		System.out.println("start tag: <" + tag + ">");
+		tagStack.push(tag);
+	}
 
-    private void matchRemainingTags() {
-        while (!tagStack.isEmpty()) {
-            String tag = tagStack.pop();
-            errorQueue.enqueue("<" + tag + "> tag is not closed properly.");
-        }
-    }
+	private void processEndTag(String tag, int lineNumber) {
+		System.out.println("end tag: </" + tag + ">");
+		if (tagStack.isEmpty()) {
+			errorQueue.enqueue("Line " + lineNumber + ": </" + tag + "> tag has no matching opening tag.");
+		} else {
+			String topTag = tagStack.peek();
+			if (!topTag.equalsIgnoreCase(tag)) {
+				if (tagStack.contains(tag)) {
+					while (!tagStack.isEmpty() && !tagStack.peek().equalsIgnoreCase(tag)) {
+						String extraTag = tagStack.pop();
+						errorQueue.enqueue("Line " + lineNumber + ": <" + extraTag
+								+ "> tag is extra and does not have a matching closing tag.");
+					}
+					if (!tagStack.isEmpty()) {
+						tagStack.pop(); // Pop the matching opening tag
+					}
+				} else {
+					errorQueue.enqueue(
+							"Line " + lineNumber + ": </" + tag + "> tag does not have a matching opening tag.");
+				}
+			} else {
+				tagStack.pop(); // Pop the matching opening tag
+			}
+		}
+	}
 
-    private void printErrors() {
-        if (errorQueue.isEmpty()) {
-            System.out.println("No errors found.");
-        } else {
-            System.out.println("Errors found:");
-            while (!errorQueue.isEmpty()) {
-                System.out.println(errorQueue.dequeue());
-            }
-        }
+	private void matchRemainingTags() {
+		while (!tagStack.isEmpty()) {
+			String tag = tagStack.pop();
+			errorQueue.enqueue("<" + tag + "> tag is not closed properly.");
+		}
+	}
 
-        System.out.println("\nParsed File Content:");
-        while (!parsedFileQueue.isEmpty()) {
-            System.out.println(parsedFileQueue.dequeue());
-        }
-    }
+	private void printErrors() {
+		if (errorQueue.isEmpty()) {
+			System.out.println("No errors found.");
+		} else {
+			System.out.println("Errors found:");
+			while (!errorQueue.isEmpty()) {
+				System.out.println(errorQueue.dequeue());
+			}
+		}
+
+		System.out.println("\nParsed File Content:");
+		while (!parsedFileQueue.isEmpty()) {
+			System.out.println(parsedFileQueue.dequeue());
+		}
+	}
 }
